4cbe3493fba1440888936b319c371642b2b696c0
samyfodil <fodil.samy@gmail.com>
2023-01-23 14:29:01 -0600

    tinygo does not like using MarshalJSON with non pointer

--- helpers.go
+++ helpers.go
@@ -3,11 +3,13 @@ package web3auth
 import "github.com/samyfodil/web3auth/proto"
 
 func MarshaledReplyError(err error) []byte {
-	bytes, _ := proto.Response{Error: err.Error()}.MarshalJSON()
+	res := &proto.Response{Error: err.Error()}
+	bytes, _ := res.MarshalJSON()
 	return bytes
 }
 
 func MarshaledInitReplyError(err error) []byte {
-	bytes, _ := proto.InitReply{Error: err.Error()}.MarshalJSON()
+	res := &proto.InitReply{Error: err.Error()}
+	bytes, _ := res.MarshalJSON()
 	return bytes
 }
